708f19739f4549f7c0423ad1f70d5f6b7976fac8
Rahul Lodhi <rahul.lodhi5003@gmail.com>
2023-07-01 23:24:24 +0530

    initial commit

---
+++ src/admin/app.js
@@ -0,0 +1,45 @@
+const config = {
+    locales: [
+        'en',
+        // 'ar',
+        // 'fr',
+        // 'cs',
+        // 'de',
+        // 'dk',
+        // 'es',
+        // 'he',
+        // 'id',
+        // 'it',
+        // 'ja',
+        // 'ko',
+        // 'ms',
+        // 'nl',
+        // 'no',
+        // 'pl',
+        // 'pt-BR',
+        // 'pt',
+        // 'ru',
+        // 'sk',
+        // 'sv',
+        // 'th',
+        // 'tr',
+        // 'uk',
+        // 'vi',
+        // 'zh-Hans',
+        // 'zh',
+    ],
+    translations: {
+        'en': {
+            'Auth.form.welcome.title': 'Welcome to TechFreelanceX',
+            'Auth.form.welcome.subtitle': 'Log in to your TechFreelanceX account',
+            // Translate a plugin's key/value pair by adding the plugin's name as a prefix
+            // In this case, we translate the "plugin.name" key of plugin "content-type-builder"
+        },
+    },
+};
+
+
+
+export default {
+    config,
+};
